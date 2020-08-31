74d91b53181ccf5d62d084346d9867bcb5db3985
laineus <lainen000@gmail.com>
2020-08-31 23:38:38 +0900

    refactoring

--- build/TileSetPlugin.js
+++ build/TileSetPlugin.js
@@ -3,6 +3,7 @@ const imagemin = require('imagemin')
 const imageminPngquant = require('imagemin-pngquant')
 const fs = require('fs')
 
+const TILE_SIZE = 32
 const DIR = './public/img/map'
 const ORIGINAL = `${DIR}/tilesets`
 const EXTRUDED = `${DIR}/extruded_tilesets`
@@ -13,7 +14,7 @@ module.exports = class {
       console.log('Begin: TileSet')
       fs.readdir(ORIGINAL, (_, files) => {
         const promises = files.map(async file => {
-          const buffer = await extrudeTilesetToBuffer(32, 32, `${ORIGINAL}/${file}`)
+          const buffer = await extrudeTilesetToBuffer(TILE_SIZE, TILE_SIZE, `${ORIGINAL}/${file}`)
           const minifiedBuffer = await imagemin.buffer(buffer, {
             plugins: [
               imageminPngquant()
