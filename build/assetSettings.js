c5d1614e2de05ecd51b0f7b7e5b36e79b3a2d524
laineus <lainen000@gmail.com>
2020-09-01 00:40:54 +0900

    mv dir

--- build/assetSettings.js
+++ build/assetSettings.js
@@ -8,8 +8,8 @@ module.exports = [
   { type: 'image', prefix: 'chara_sprite/', dir: '/img/chara_sprites', rule: /^\w+\.png$/ },
   { type: 'image', prefix: 'tileset/', dir: '/img/map/tilesets', rule: /^\w+\.png$/, callback: replaceToExtrudedTiles },
   { type: 'image', prefix: 'tileset/', dir: '/img/map/images', rule: /^\w+\.png$/ },
-  { type: 'json', prefix: '', dir: '/map', rule: /^\w+\D\.json$/ },
-  { type: 'tilemapTiledJSON', prefix: '', dir: '/map', rule: /^\w+\d+\.json$/ },
+  { type: 'json', prefix: '', dir: '/map/tileset_settings', rule: /^\w+\.json$/ },
+  { type: 'tilemapTiledJSON', prefix: '', dir: '/map', rule: /^\w+\.json$/ },
   { type: 'audio', prefix: 'se/', dir: '/audio/se', rule: /^\w+\.wav$/ },
   { type: 'audio', prefix: 'bgm/', dir: '/audio/bgm', rule: /^\w+\.(m4a|ogg)$/ }
 ]
