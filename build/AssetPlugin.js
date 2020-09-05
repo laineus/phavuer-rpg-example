fa1715e30675337cf1a6d8f1b9c04f4c33d2dfd8
laineus <lainen000@gmail.com>
2020-09-05 15:42:47 +0900

    refactoring

--- build/AssetPlugin.js
+++ build/AssetPlugin.js
@@ -3,16 +3,10 @@ const sizeOf = require('image-size')
 const path = require('path')
 
 const defaultSettings = {
-  documentRoot: 'public',
-  spriteSheetSettingsFileName: 'settings.json'
-}
-
-const getSpriteSheetOption = (filePath, numOfX, numOfY) => {
-  const { width, height } = sizeOf(filePath)
-  const frameWidth = Math.round(width / numOfX)
-  const frameHeight = Math.round(height / numOfY)
-  const endFrame = numOfX * numOfY
-  return { frameWidth, frameHeight, endFrame }
+  documentRoot: '/public',
+  importName: 'assets',
+  spriteSheetSettingsFileName: 'settings.json',
+  jsonOutputPath: './.assets.json'
 }
 
 module.exports = class {
@@ -27,39 +21,38 @@ module.exports = class {
   afterEnvironment (compiler) {
     console.log('AssetsPlugin: Initializing...')
     const data = this.getAssetsData()
-    compiler.options.externals.assets = JSON.stringify(data)
-    fs.writeFileSync('./.assets.json', JSON.stringify(data, null, '  '))
+    compiler.options.externals[this.settings.importName] = JSON.stringify(data)
+    fs.writeFileSync(this.settings.jsonOutputPath, JSON.stringify(data, null, '  '))
     console.log('AssetsPlugin: Initialized!')
   }
   afterCompile (compilation) {
-    compilation.fileDependencies.add(path.resolve('./.assets.json'))
-    fs.watch('./public/img/sprites', event => {
+    compilation.fileDependencies.add(path.resolve(this.settings.jsonOutputPath))
+    fs.watch(`.${this.settings.documentRoot}/img/sprites`, event => {
       if (event !== 'rename') return
-      const assetsModule = compilation.modules.find(v => v.userRequest === 'assets')
+      const assetsModule = compilation.modules.find(v => v.userRequest === this.settings.importName)
       if (!assetsModule) return
       console.log('AssetsPlugin: Updating...')
       const data = this.getAssetsData()
       assetsModule.request = JSON.stringify(data)
-      fs.writeFileSync('./.assets.json', JSON.stringify(data, null, '  '))
+      fs.writeFileSync(this.settings.jsonOutputPath, JSON.stringify(data, null, '  '))
       console.log('AssetsPlugin: Updated!')
     })
   }
   getAssetsData () {
     const data = this.patterns.reduce((result, pattern) => {
-      const dir = `./public/${pattern.dir}`
-      const files = fs.readdirSync(dir)
-      const spriteSheetSettings = this.getSpriteSheetSttings(dir)
-      const list = files.filter(file => pattern.rule.test(file)).reduce((list, file) => {
-        const fileWithoutExt = file.split('.')[0]
-        const key = `${pattern.prefix}${fileWithoutExt}`
-        const path = `.${pattern.dir}/${file}`
+      const dir = `.${this.settings.documentRoot}/${pattern.dir}`
+      const fileNames = fs.readdirSync(dir)
+      const spriteSheetSettings = this.getSpriteSheetSettings(dir)
+      const list = fileNames.filter(fileName => pattern.rule.test(fileName)).reduce((list, fileName) => {
+        const key = `${pattern.prefix}${fileName.split('.')[0]}`
+        const path = `.${pattern.dir}/${fileName}`
         const sameKeyRow = list.find(v => v[0] === key)
         if (sameKeyRow) {
           // Apend file if existing same key
           sameKeyRow.splice(1, 1, [sameKeyRow[1], path].flat())
         } else {
-          const spriteSheetSetting = spriteSheetSettings && spriteSheetSettings.find(v => v[0] === fileWithoutExt)
-          const spriteSheetOption = spriteSheetSetting && getSpriteSheetOption(`${dir}/${file}`, spriteSheetSetting[1], spriteSheetSetting[2])
+          const spriteSheetSetting = spriteSheetSettings && spriteSheetSettings.find(v => v[0] === fileName)
+          const spriteSheetOption = spriteSheetSetting && this.getSpriteSheetOption(`${dir}/${fileName}`, spriteSheetSetting[1], spriteSheetSetting[2])
           list.push(spriteSheetOption ? [key, path, spriteSheetOption] : [key, path])
         }
         return list
@@ -72,7 +65,7 @@ module.exports = class {
     data.image = data.image.filter(v => v.length === 2)
     return data
   }
-  getSpriteSheetSttings (dir) {
+  getSpriteSheetSettings (dir) {
     const pathToSettings = `${dir}/${this.settings.spriteSheetSettingsFileName}`
     if (!fs.existsSync(pathToSettings)) return null
     const settingsJson = fs.readFileSync(pathToSettings, 'utf8')
@@ -82,5 +75,12 @@ module.exports = class {
       console.error(`[Error] Invalid JSON String: ${pathToSettings}`)
       return null
     }
+  }
+  getSpriteSheetOption (filePath, numOfX, numOfY) {
+    const { width, height } = sizeOf(filePath)
+    const frameWidth = Math.round(width / numOfX)
+    const frameHeight = Math.round(height / numOfY)
+    const endFrame = numOfX * numOfY
+    return { frameWidth, frameHeight, endFrame }
   }
 }
