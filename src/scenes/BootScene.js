9d63564ec42a1aa9ec1252aac11562e98c783035
laineus <lainen000@gmail.com>
2020-09-17 00:30:43 +0900

    remove registerAnims

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,12 +1,10 @@
 import loadAssets from '@/util/loadAssets'
-import registerAnims from '@/util/registerAnims'
 export default class BootScene extends Phaser.Scene {
   constructor () {
     super({ key: 'Boot', active: true })
   }
   create () {
     Phaser.BlendModes.OVERLAY = this.sys.game.renderer.addBlendMode([WebGLRenderingContext.SRC_ALPHA, WebGLRenderingContext.ONE], WebGLRenderingContext.FUNC_ADD)
-    registerAnims(this)
     this.scene.start('UI')
     this.scene.start('Game', { map: 'room1', x: 610, y: 340 })
   }
--- src/util/registerAnims.js
+++
@@ -1,3 +0,0 @@
-export default scene => {
-  scene.anims.create({ key: 'treasure', frames: scene.anims.generateFrameNumbers('chara_sprite/treasure_chest', { start: 0, end: 6 }), repeat: 0, frameRate: 20 })
-}
