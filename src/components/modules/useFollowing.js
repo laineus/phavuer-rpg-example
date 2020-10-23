d6179aa7eb75b396f1acad56bc2ed0d847bb1f8e
laineus <lainen000@gmail.com>
2020-10-24 00:06:44 +0900

    follow target

--- src/components/modules/useFollowing.js
+++ src/components/modules/useFollowing.js
@@ -1,10 +1,12 @@
-import { computed, reactive, unref } from 'vue'
+import { computed, ref, reactive, unref } from 'vue'
 
 export default chara => {
   const targetPosition = reactive({
     x: null,
     y: null
   })
+  const targetObject = ref(null)
+  const setTargetObject = object => targetObject.value = object
   let resolver = null
   const setTargetPosition = (x, y) => {
     targetPosition.x = x
@@ -23,10 +25,12 @@ export default chara => {
   const getDiffToTargetPositionY = () => hasTargetPosition.value ? targetPosition.y - unref(chara).y : 0
   const getDiffToTargetPositionDistance = (x, y) => Math.hypot(x || getDiffToTargetPositionX(), y || getDiffToTargetPositionY())
   const walkToTargetPosition = speed => {
+    if (targetObject.value) setTargetPosition(targetObject.value.x, targetObject.value.y)
     if (!hasTargetPosition.value) return
     const diffX = getDiffToTargetPositionX()
     const diffY = getDiffToTargetPositionY()
     const distance = getDiffToTargetPositionDistance(diffX, diffY)
+    if (targetObject.value && distance < 50) return clearTargetPosition()
     const body = chara.value.body
     const x = (!body.blocked.left && !body.blocked.right) ? diffX : diffX * 0.1
     const y = (!body.blocked.top && !body.blocked.down) ? diffY : diffY * 0.1
@@ -43,6 +47,8 @@ export default chara => {
     getDiffToTargetPositionX,
     getDiffToTargetPositionY,
     getDiffToTargetPositionDistance,
-    walkToTargetPosition
+    walkToTargetPosition,
+    targetObject,
+    setTargetObject
   }
 }
