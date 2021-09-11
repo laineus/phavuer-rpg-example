3fe1e57fb30c8cf98134510f7c9d410ef543ef98
Laineus <lainen000@gmail.com>
2021-09-11 18:41:37 +0900

    setup vite

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import replace from '@rollup/plugin-replace'
+import phaserAssetsLoader from 'phaser-assets-loader'
+import assetsConfig from './assets.config.js'
+import packageJson from './package.json'
+
+const vitePhaserAssetsLoader = config => {
+  return {
+    name: 'phaser-assets-loader',
+    config (_, { mode }) {
+      const { exportJson, watch } = phaserAssetsLoader(config)
+      exportJson()
+      if (mode === 'development') {
+        watch()
+      }
+    }
+  }
+}
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vitePhaserAssetsLoader(assetsConfig),
+    replace({
+      'APP_VERSION': JSON.stringify(packageJson.version),
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true)
+    })
+  ]
+})
